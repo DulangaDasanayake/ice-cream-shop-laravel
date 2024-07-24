8431bb437014afb2de1aac873ad926ceb7f5fdd8
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-24 16:52:54 +0530

    Products table

---
+++ database/migrations/2024_07_24_104740_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price',8,2); //999999.00
+            $table->decimal('sale_price',8,2);
+            $table->integer('quantity');
+            $table->string('category');
+            $table->string('type');
+            $table->string('image');
+            $table->string('image1')->nullable();
+            $table->string('image2')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
