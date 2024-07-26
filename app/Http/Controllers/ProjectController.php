248a8855705b0f52ed6d4453d7a0efbd6e6fd7e3
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-26 13:10:51 +0530

    ProjectController update

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
+use \Illuminate\Support\Facades\DB;
+
 class ProjectController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $products = DB::table('products')->get();
+        return view('index', ['products' => $products]);
+    }
+
+    public function products()
+    {
+        $products = DB::table('products')->get();
+        return view('products', ['products' => $products]);
     }
 }
