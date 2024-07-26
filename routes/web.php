ff4e572f4796863c6a3e075a29658f33dc91226c
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-26 13:10:07 +0530

    Routes updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [ProjectController::class, 'index'])->name('home');
+Route::get('/products', [ProjectController::class, 'products'])->name('products');
