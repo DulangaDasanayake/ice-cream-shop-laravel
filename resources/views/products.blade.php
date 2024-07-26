cac7fb37c0db619e7ec5f5a686176f8fd6700284
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-26 23:20:33 +0530

    products blade update

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,35 +10,39 @@
                 </div>
             </div>
             <div class="row">
-
                 @foreach ($products as $product)
-                    <div class="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
-                        <div class="bg-primary mt-n5 py-3" style="width: 80px;">
+                    <div class="col-12">
+                        <div class="owl-carousel product-carousel">
 
-                            @if ($product->sale_price != null)
-                                <h4 class="font-weight-bold text-white mb-0">${{ $product->sale_price }}</h4>
-                                <h4 class="font-weight-bold text-white mb-0" style="text-decoration: line-through;">
-                                    ${{ $product->price }}</h4>
-                            @else
-                                <h4 class="font-weight-bold text-white mb-0">${{ $product->sale_price }}</h4>
-                            @endif
+                            <div
+                                class="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
+                                <div class="bg-primary mt-n5 py-3" style="width: 80px;">
 
-                        </div>
-                        <div class="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3"
-                            style="width: 150px; height: 150px;">
-                            <img class="rounded-circle w-100 h-100" src="{{ asset('img/' . $product->image) }}"
-                                style="object-fit: cover;">
-                        </div>
-                        <h5 class="font-weight-bold mb-4">{{ $product->name }}</h5>
-                        <a href="" class="btn btn-sm btn-secondary">Order Now</a>
-                    </div>
+                                    @if ($product->sale_price != null)
+                                        <h4 class="font-weight-bold text-white mb-0">${{ $product->sale_price }}</h4>
+                                        <h4 class="font-weight-bold text-white mb-0" style="text-decoration: line-through;">
+                                            ${{ $product->price }}</h4>
+                                    @else
+                                        <h4 class="font-weight-bold text-white mb-0">${{ $product->sale_price }}</h4>
+                                    @endif
+
+                                </div>
+                                <div class="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3"
+                                    style="width: 150px; height: 150px;">
+                                    <img class="rounded-circle w-100 h-100" src="{{ asset('img/' . $product->image) }}"
+                                        style="object-fit: cover;">
+                                </div>
+                                <h5 class="font-weight-bold mb-4">{{ $product->name }}</h5>
+                                <a href="" class="btn btn-sm btn-secondary">Order Now</a>
+                            </div>
                 @endforeach
-
-                <div class="col-12 text-center">
-                    <a href="" class="btn btn-primary py-3 px-5">Load More</a>
-                </div>
             </div>
         </div>
+        <div class="col-12 text-center">
+            <a href="" class="btn btn-primary py-3 px-5">Load More</a>
+        </div>
+    </div>
+    </div>
     </div>
     <!-- Products End -->
 @endsection
