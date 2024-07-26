355fa085b30433ffd6df3049c17d4198a2470956
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-26 23:19:56 +0530

    single_product controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-use \Illuminate\Support\Facades\DB;
 
 class ProjectController extends Controller
 {
@@ -17,5 +18,11 @@ class ProjectController extends Controller
     {
         $products = DB::table('products')->get();
         return view('products', ['products' => $products]);
+    }
+
+    public function single_product(Request $request, $id)
+    {
+        $product_array = DB::table('products')->where('id', $id)->get();
+        return view('single_product', ['product_array' => $product_array]);
     }
 }
