30a26714ec26b20a9b82d9d37f1447c110b44600
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-30 17:57:01 +0530

    Bug fix

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,27 +5,24 @@
         <div class="container py-5">
             <div class="row justify-content-center">
                 <div class="col-lg-6">
-                    <h1 class="section-title position-relative text-center mb-5">Best Prices We Offer For Ice Cream Lovers
+                    <h1 class="section-title position-relative text-center mb-5">
+                        Best Prices We Offer For Ice Cream Lovers
                     </h1>
                 </div>
             </div>
             <div class="row">
-                @foreach ($products as $product)
-                    <div class="col-12">
-                        <div class="owl-carousel product-carousel">
-
-                            <div
-                                class="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
+                <div class="col-12">
+                    <div class="owl-carousel product-carousel">
+                        @foreach ($products as $product)
+                            <div class="product-item d-flex flex-column align-items-center text-center bg-light rounded py-5 px-3">
                                 <div class="bg-primary mt-n5 py-3" style="width: 80px;">
-
                                     @if ($product->sale_price != null)
                                         <h4 class="font-weight-bold text-white mb-0">${{ $product->sale_price }}</h4>
                                         <h4 class="font-weight-bold text-white mb-0" style="text-decoration: line-through;">
                                             ${{ $product->price }}</h4>
                                     @else
-                                        <h4 class="font-weight-bold text-white mb-0">${{ $product->sale_price }}</h4>
+                                        <h4 class="font-weight-bold text-white mb-0">${{ $product->price }}</h4>
                                     @endif
-
                                 </div>
                                 <div class="position-relative bg-primary rounded-circle mt-n3 mb-4 p-3"
                                     style="width: 150px; height: 150px;">
@@ -42,19 +39,17 @@
                                     <input type="hidden" name="sale_price" value="{{ $product->sale_price }}">
                                     <input type="hidden" name="quantity" value="1">
                                     <input type="hidden" name="image" value="{{ $product->image }}">
-                                    <input type="submit" value="add to cart">
+                                    <input type="submit" value="Add to Cart">
                                 </form>
-
-
                             </div>
-                @endforeach
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 text-center">
+                <a href="#" class="btn btn-primary py-3 px-5">Load More</a>
             </div>
         </div>
-        <div class="col-12 text-center">
-            <a href="" class="btn btn-primary py-3 px-5">Load More</a>
-        </div>
-    </div>
-    </div>
     </div>
     <!-- Products End -->
 @endsection
