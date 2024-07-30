687f0568fc96bb9d4b0f4441542fe753ea91d199
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-30 18:04:46 +0530

    Payment design

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
+    <section class="container mt-2 my-3 py-5">
+        <div class="container mt-2 text-center">
 
-<h1>Payment</h1>
+            <h4>Payment</h4>
 
+            @if (Session::has('total') && Session::get('total') != null)
+                @if (Session::has('order_id') && Session::get('order_id') != null)
+                    <h4 style="color: blue" class="my-5">Total: ${{Session::get('total')}}</h4>
+                @endif
+            @endif
+            
+        </div>
+    </section>
 @endsection
